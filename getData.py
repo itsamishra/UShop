49e6a44bd795aa034212396bb0a2482b2ca7da84
AM <ashutoshmishra413@gmail.com>
2017-08-15 20:24:47 -0400

    Added Amazon.com + Walmart.com support

--- getData.py
+++ getData.py
@@ -1,8 +1,9 @@
 import urllib
 from bs4 import BeautifulSoup as soup
 
-def getAmazonData(URL):
-	amazonURL = URL
+# Gets price of amazon item with associated URL (sometimes throws error as connection isn't thred-safe)
+def getAmazonData(UPC):
+	amazonURL = "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=" + str(UPC)
 
 	opener = urllib.request.build_opener()
 	opener.addheaders = [('User-agent', 'Mozilla/5.0')]	# used to spoof Amazon
@@ -16,3 +17,23 @@ def getAmazonData(URL):
 	costCents = productInfo.find("sup", {"class":"sx-price-fractional"}).text
 
 	return str(costDollars) + "." + str(costCents)
+
+def getWalmartData(UPC):
+	walmartURL = "https://www.walmart.com/search/?query=" + str(UPC)
+
+	opener = urllib.request.build_opener()
+	opener.addheaders = [('User-agent', 'Mozilla/5.0')]	# used to spoof Amazon
+	response = opener.open(walmartURL)
+	pageHTML = response.read()
+
+	pageSoup = soup(pageHTML, "html.parser")
+	productInfo = pageSoup.findAll("div", {"class":"search-result-listview-item clearfix"})[0]
+
+	costDollars = productInfo.find("span", {"class":"Price-characteristic"}).text
+	costCents = productInfo.find("span", {"class":"Price-mantissa"}).text
+
+	return str(costDollars) + "." + str(costCents)
+
+
+
+print(getWalmartData("037000509783"))
--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import getData
 
 def main():
-	amazonPrice = getData.getAmazonData("https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=037000509783&rh=i%3Aaps%2Ck%3A037000509783")
+	UPC = input()
+
+	# Gets Amazon price for product
+	try:
+		amazonPrice = getData.getAmazonData(UPC)
+	except:
+		amazonPrice = getData.getAmazonData(UPC)
+
 	print(amazonPrice)
 
 if __name__ == '__main__':
